f023d2aef97f3b1236de4d896f37e39216a01903
jcraane <jcraane@gmail.com>
2014-01-19 10:27:45 +0100

    Removed unused code and coment.

--- BundleStateLib/src/main/java/nl/capaxit/bundlestatelib/state/activity/BundleStateActivity.java
+++ BundleStateLib/src/main/java/nl/capaxit/bundlestatelib/state/activity/BundleStateActivity.java
@@ -7,12 +7,9 @@ import nl.capaxit.bundlestatelib.state.annotations.BundleStateAnnotationProcesso
 /**
  * Insert documentation here.
  *
- * TODO: Add annotation cache and separate the processing from the activity/lifecycle.
  * @author jcraane
  */
 public class BundleStateActivity extends Activity{
-    private static final String TAG = BundleStateActivity.class.getSimpleName();
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
